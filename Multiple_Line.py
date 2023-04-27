4d18b35a341d3eed6f74d5ce589762bf0b899479
unknown <anonuevoloraine6@gmail.com>
2023-04-27 15:50:01 +0800

    To open the file mylife.txt If the file does not already exist, it will be created

--- Multiple_Line.py
+++ Multiple_Line.py
@@ -13,3 +13,15 @@ print("\033[1;3mMy name is \033[;45;1;3mLoraine\033[0m")
 your_name = input("\033[1;3mWhat is your name?\033[0m")
 print("\033[;1;3mI'm glad that you're here!\033[;34;1;3m" + your_name + "\033[0m \033[;1;3m, sit back and learn with me!\033[0m")
 
+#To open the file "mylife.txt" If the file does not already exist, it will be created.
+with open("mylife.txt", "w") as file:
+
+    #Use loop to execute the block of code repeatedly.
+    while True:
+        #Request for a line of text from the user.
+        line = input("\n\033[;34;1;3mInput line: \033[0m")
+        #Insert the text line, followed by a new line character, into the file.
+        file.write(f" {line}\n")
+
+#Close the file
+file.close()
